94ad44f5e2007fb8e339dd8115319bf8fefb2a01
JACQUELINE GENOVEZ <167133184+jacquelineterraboa@users.noreply.github.com>
2024-10-18 00:01:45 +0000

    Inseri pasta de graficos e arquivo de informacoesGlobais.js

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,8 @@
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/esportes/esportes-dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados)
+}
+vizualizarInformacoesGlobais()
