aa0bae1d2afe1770e9d52decc469c3eeca4257e7
JACQUELINE GENOVEZ <167133184+jacquelineterraboa@users.noreply.github.com>
2024-10-31 23:55:59 +0000

    criei o css

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,5 +4,12 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas. Desse total <span> ${dados.total_praticam_esporte} </span> regularmente. O tempo médio praticado semanalmente é de aproximadamente <span> ${dados.total_tempo_médio} </span> em contra partida temos <span> ${dados.total_tempo_medio} </span> de pessoas que assintem esporte jovens`
+    const container = document.getElementById('graficos-container')
+    container.app
+container.appendChild(paragrafo);
 }
+
 vizualizarInformacoesGlobais()
