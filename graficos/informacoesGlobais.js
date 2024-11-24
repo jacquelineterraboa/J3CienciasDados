df6a3c071bdcb52141e6e1610589acc4d793874b
JACQUELINE GENOVEZ <167133184+jacquelineterraboa@users.noreply.github.com>
2024-11-24 01:10:47 +0000

    montagem

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,15 +1,20 @@
-const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/esportes/esportes-dados-globais.json'
+const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas. Desse total <span> ${dados.total_praticam_esporte} </span> regularmente. O tempo médio praticado semanalmente é de aproximadamente <span> ${dados.total_tempo_médio} </span> em contra partida temos <span> ${dados.total_tempo_medio} </span> de pessoas que assintem esporte jovens`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+
     const container = document.getElementById('graficos-container')
-    container.app
-container.appendChild(paragrafo);
+    container.appendChild(paragrafo)
 }
 
 vizualizarInformacoesGlobais()
